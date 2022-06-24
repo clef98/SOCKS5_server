960ff80f8b08979d4aedba406a428bf2c14c0ba5
ericsong <ericysong007@gmail.com>
2022-06-24 18:57:46 -0400

    finished compile errors.

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,57 @@
 use std::io::Write;
-use std::iter;
 use std::io;
-use std::net::{IpAddr, Shutdown, TcpStream};
+use std::net::{TcpStream, TcpListener};
+use std::io::Read;
 
-fn socket_IP(address: std::net::SocketAddr) -> Vec<u8> {
-    let mut output = Vec::new();
-    let ip_vector = match address.ip() {
-        IpAddr::V4(ip) => {
-            output.push(0x01);
-            ip.octets().to_vec()
-        }
-        IpAddr::V6(ip) => {
-            address.push(0x04);
-            ip.octects().to_vec();
-        }
-    };
-    for i in ip_vector.iter() {
-        output.push(*i);
-    }
-    output.put_u16(address.port());
-    output
-}
-
-fn read_packets(address: String, mut stream: TcpStream) -> io::Result<()>{
-    let mut reader = stream.clone();
-    let mut writer = stream;
+fn handle_connection(address: String, mut stream: TcpStream) -> io::Result<()>{
     let mut buffer = vec![0u8; 512];
-    reader.read_exact(&mut buffer[0..2]);
+    stream.read(&mut buffer[0..2]).expect("TODO: panic message");
     if buffer[0] != 0x05 {
-        return Err(std::io::Error::new(std::io::ErrorKind::ConnectionAborted, "only socks5 protocol is supported!",));
+        return Err(std::io::Error::new(std::io::ErrorKind::ConnectionAborted, "Only socks5 protocol is supported!"));
     }
-    writer.write(&[0x05u8, 0x00]).flush();
-    reader.read_exact(&mut buffer[0..4]).await?;
-    let cmd = buffer[1];
+    if buffer[1] != 0x01 {
+        return Err(std::io::Error::new(std::io::ErrorKind::ConnectionAborted, "Only connect cmd is supported!"));
+    }
+    stream.write(&[0x05u8, 0x00]).expect("Error with writing to stream.");
+    stream.read(&mut buffer[0..4]).expect("Error with reading stream.");
     //UDP ASSOCIATE AND BIND ARE NOT SUPPORTED.
     let mut port_type = buffer[3];
     let mut flag = true;
-    let mut port = new::String();
+    let mut port = String::new();
     match port_type {
-        Ok(0x01) => {
-            reader.read_exact(&mut buffer[0..6]).flush();
-            println("Port has been selected.");
+        0x01 => {
+            stream.read_exact(&mut buffer[0..6]).expect("TODO: panic message");
+            println!("Port has been selected.");
         }
-        Ok(0x02) => {
+        0x02 => {
             std::io::ErrorKind::ConnectionAborted;
         }
-        Ok(0x03) => {
-            reader.read_exact(&mut buffer[0..1]).flush();
+        0x03 => {
+            stream.read_exact(&mut buffer[0..1]).expect("TODO: panic message");
         }
-        Ok(0x04) => {
-            reader.read_exact(&mut buffer[0..18]).flush();
+        0x04 => {
+            stream.read_exact(&mut buffer[0..18]).expect("TODO: panic message");
         }
-        Err(_) => {
+
+        _ => {
+            println!("Failed connection");
             flag = false;
+            std::process::exit(1);
         }
     }
     Ok(())
 }
 
-
-fn main() -> io::Result<()> {
+fn main() -> std::io::Result<()> {
     println!("Please type the address you would like to connect to: ");
-    let args: Vec<String> = env::args().collect();
-    let address: &str = &args[1];
-    let listener = TcpListener::bind(address);
+    let args: Vec<String> = std::env::args().collect();
+    let address:&str  = &args[1];
+    let listener = TcpListener::bind(address).unwrap();
     for stream in listener.incoming() {
+        let address_clone = address.to_string().clone();
         match stream {
             Ok(stream) => {
-                thread::spawn(move || { read_packets(address as String, stream) });
+                std::thread::spawn(move || { handle_connection(address_clone, stream) });
             }
             Err(e) => {
                 println!("Failed to receive messages: {}", e);
